7d0bacf7265241616b9f9ebc38fafb7ef9430972
Sedrak Nadzharyan <sedrak005@gmail.com>
2022-03-19 15:30:22 -0400

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <h1>Hello</h1>
+      <h2>Hello</h2>
+      <h3>Hello</h3>
+      <p>Hello</p>
+    </nav>
+  );
+}
+
+export default Navbar;
